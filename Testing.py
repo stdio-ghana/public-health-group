fd802956c976e561585741a38e66eb0b800128e3
gafar-yahaya <gafar.yahaya@gmail.com>
2014-06-17 16:21:42 +0000

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -2,3 +2,21 @@
 -Test to check whether available data is enough for processing.
 -Test to check whether the program is running with thae right data-types.
 
+****TEST FUNCTIONS****
+
+def readData():
+  return data[]
+
+
+
+def test_enoughData():
+  assert readData() == data[]
+
+def dataType():
+  assert readData() == int()
+  
+  
+  
+  
+  
+  
