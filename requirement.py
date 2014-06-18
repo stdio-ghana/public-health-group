47f7adc057ce8865948cc73aabea404dc29f5691
skillfulfranco <skillfulfranco@gmail.com>
2014-06-18 06:59:46 +0100

    updates

--- requirement.py
+++ requirement.py
@@ -1,15 +1,18 @@
-We have spelt out the functional and non-fuctional requirement our software will need to function.
+We have spelt out the functional and non-fuctional requirement our software
+will need to function.
 
 Fuctional Requirements: With the assumption that we already have the data.
-* Loads the data onto an array in the system.
+* Loads the data onto a list in the system.
 * Generates a table for the available data.
 * Generates a plot from the loaded data (pie-chart, bar, time-series).
 * Further generates a brief statistical interpretation of the plot.
 
 Non-Functional Requirements:
-* Interacts with the central system of the hospitals to get information on the related disease within a specific time frame.
+* Interacts with the central system of the hospitals to get information on the
+related disease within a specific time frame.
 
 
 Use Case:
-A user should be able to query to have a short forecast report depicting some statistics on the data.
+A user should be able to query to have a short forecast report depicting some
+statistics on the data.
 
