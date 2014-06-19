5106505836673dc9b01900d57d2b6b9e5a58faca
skillfulfranco <skillfulfranco@users.noreply.github.com>
2014-06-19 22:57:21 +0100

    Update README.py

--- README.py
+++ README.py
@@ -1,13 +1,13 @@
 Public health is the science of protecting and improving the health of community through education, promotion of healthy lifestyles and research for
-the disease and injury prevention.Public health professionals analyze the effect on health of genetic, personal choice and the envionment in
+the disease and injury prevention. Public health professionals analyze the effect on health of genetic, personal choice and the envionment in
 order to develop programs that protect the health of people and community.
 Overall, public health is concerned with protecting the health of entire populations. These populations can be small as a local neighbourhood,
 or as big as an entire country.
 
 Public health report gives a picture of the health status of the given area at a particular point in time.
 Our software named the "health-REPORTER" is a software tool which we intend to design to aid health managements to collect,
-compile, analyze and generate reports monitoring the growth of a particular disease among selected hospitals.Its primary aim is to give a
-brief report on the prevalance of a particular disease within different locations at different seasonal periods.
+compile, analyze and generate reports monitoring the growth of diseases from selected hospitals. Its primary aim is to give a
+brief report on the prevalence of a particular disease within different locations at different seasonal periods.
 
 We intend to start small by just producing a software that can take some data relating to a particular disease and generate statistical plots based on that data.
 The software should subsequently be able to add other features to its primary feature such as giving a brief interpretation of the plot
